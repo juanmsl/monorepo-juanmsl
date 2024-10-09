be8136ec13d57c710dd2eabbc7b83fd145d16b2d
Juan Sánchez <juanmsl.pk@gmail.com>
2024-10-08 21:08:16 -0500

    Creating branch storybook-update

--- packages/ui/.storybook/main.ts
+++ packages/ui/.storybook/main.ts
@@ -23,19 +23,23 @@ const config: StorybookConfig = {
       files: '**/*.@(mdx|stories.@(mdx|js|jsx|mjs|ts|tsx))',
     },
   ],
-  addons: [
-    {
-      name: getAbsolutePath('@storybook/addon-essentials'),
-      options: {
-        actions: false,
-      },
+
+  addons: [{
+    name: getAbsolutePath('@storybook/addon-essentials'),
+    options: {
+      actions: false,
     },
-    getAbsolutePath('@storybook/addon-links'),
-    getAbsolutePath('@storybook/addon-interactions'),
-  ],
+  }, getAbsolutePath('@storybook/addon-links'), getAbsolutePath('@storybook/addon-interactions'), getAbsolutePath("@storybook/addon-mdx-gfm"), '@chromatic-com/storybook'],
+
   framework: {
     name: getAbsolutePath('@storybook/react-vite'),
     options: {},
   },
+
+  docs: {},
+
+  typescript: {
+    reactDocgen: 'react-docgen-typescript'
+  }
 };
 export default config;
